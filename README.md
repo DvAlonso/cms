380d5634b3c00d0d0d8d288dcedb934af0d72cbe
David Alonso <40397323+DvAlonso@users.noreply.github.com>
2022-06-22 11:58:55 +0200

    Initialize dev

---
+++ README.md
@@ -0,0 +1,3 @@
+# Base template
+
+This is base template with Livewire, AlpineJS and TailwindCSS installed and configured, ready to start developing.
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  darkMode: "class",
+  content: ["./resources/**/*.blade.php"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
